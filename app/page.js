4886d525edd5d663bd96b6ee31e87bca39fde6bd
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-01-25 18:11:33 +0600

    error handling done

--- app/page.js
+++ app/page.js
@@ -27,8 +27,10 @@ export default function Page() {
 
   return (
     <div className="container">
-      <h1 className="text-[22px] font-bold mt-1">AI Chat App</h1>
-      <div className="flex flex-col h-screen">
+      <div className="flex flex-col h-screen relative">
+        <h1 className="text-[22px] font-bold px-2 py-2 sticky top-0 left-0">
+          AI Chat App
+        </h1>
         <div className="flex-1 overflow-y-auto p-4">
           {messages.map((msg, id) => (
             <ChattingMassege
